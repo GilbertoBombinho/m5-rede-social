7894c876d9ef417f15bc8a32c17c9062340d0400
Gilberto <gilberto.bombinho.cardoso@gmail.com>
2023-07-10 14:47:16 -0300

    fix users list

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from .models import Post
+from comments.serializers import CommentSerializer
 
 
 class PostSerializer(serializers.ModelSerializer):
+    posts_comment = CommentSerializer(many=True, read_only=True)
     class Meta:
         model = Post
-        fields = ["id", "title", "content", "privacy","user_id", "like_id"]
+        fields = ["id", "title", "content", "privacy","user_id", "like_id", "posts_comment"]
 
         
         
